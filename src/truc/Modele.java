e0935e92036a3c9b7377a9b7476afe69639dae42
Phenca <soran@LORDI-51QDLSFOO.localdomain>
2023-02-08 08:26:47 +0100

    Début

--- src/truc/Modele.java
+++ src/truc/Modele.java
@@ -4,7 +4,8 @@ import java.util.Observable;
 
 public class Modele extends Observable {
 
-	
+	public String COULEURS[] = {"Jaune", "Bleu", "Vert", "Magenta", "Rouge", "Orange", "Blanc", "Noir"};
+	public int N_TENTATIVES = 10;
 
 	
 }
